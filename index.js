d275ffc424ccf4930e9950fd25ca02448ac2bf23
YUBIIX <150880583+YUBIIX@users.noreply.github.com>
2024-02-22 23:18:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,416 @@
+var product = [{
+    id: 1,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb7MoaPxZE_tsrn_5u2MgoxD5CLu4tATFdrg&usqp=CAU',
+    name: 'Hoyt straros 40 SVX',
+    price: 69000,
+    description: 'Hoyt Straros 40 SVX Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Compound'
+}, {
+    id: 2,
+    img: 'https://www.merlinarchery.co.uk/media/catalog/product/cache/0f69a8dcabec5028f38e23ff1b945685/8/3/838062_2.jpg',
+    name: 'Hoyt altus DCX',
+    price: 39500,
+    description: 'Hoyt Alyus DCX Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Compound'
+}, {
+    id: 3,
+    img: 'https://cdn11.bigcommerce.com/s-ih6axy/products/12284/images/10697/Optifade_Subalpine__50203.1700089838.600.600.png?c=2',
+    name: 'Hoyt alpha X 30',
+    price: 49500,
+    description: 'Hoyt Alpha X 30 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Compound'
+}, {
+    id: 4,
+    img: 'https://lancasterarchery.com/cdn/shop/files/1036475_origin_grande.jpg?v=1699390389',
+    name: 'Hoyt alpha X 33',
+    price: 52500,
+    description: 'Hoyt Alpha X 33 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Compound'
+}, {
+    id: 5,
+    img: 'https://i0.wp.com/whaletalesarchery.com/wp-content/uploads/2023/11/rx8ultra-orgin.webp?fit=1000%2C1000&ssl=1',
+    name: 'Hoyt carbon RX-8',
+    price: 67000,
+    description: 'Hoyt Carbon RX-8 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Compound'
+}, {
+    id: 6,
+    img: 'https://walesarchery.com/cdn/shop/products/image_2a27fafd-d5ec-419e-9ad4-9ade1fe4f583_2000x.png?v=1694952614',
+    name: 'Hoyt stratos 40 HBT',
+    price: 69000,
+    description: 'Hoyt Stratos 40 HBT Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Compound'
+}, {
+    id: 7,
+    img: 'https://i0.wp.com/whaletalesarchery.com/wp-content/uploads/2023/11/rx8ultra-orgin.webp?fit=1000%2C1000&ssl=1',
+    name: 'Hoyt carbon RX-8 ULTRA',
+    price: 69000,
+    description: 'Hoyt Carbon RX-8 ULTRA Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Compound'
+}, {
+    id: 8,
+    img: 'https://scheels.scene7.com/is/image/Scheels/88937433478_2?wid=500&hei=500&qlt=60',
+    name: 'Hoyt torrex',
+    price: 31000,
+    description: 'Hoyt Torrex Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Compound'
+}, {
+    id: 9,
+    img: 'https://www.canadaarcheryonline.com/cdn/shop/products/Hoyt-Arcos-25-Recurve-Riser_grande.jpg?v=1642614275',
+    name: 'Hoyt arcos',
+    price: 19000,
+    description: 'Hoyt Arcos Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Recurve'
+}, {
+    id: 10,
+    img: 'https://www.hattila.com/8127-pdt_360/abnehmbarer-bogensatz-griffe-aste-seil-winwin-wiawis-cx7.jpg',
+    name: 'WIN&WIN wiawis CX-7 riser',
+    price: 23500,
+    description: 'WIN&WIN wiawis CX-7 RISER Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Recurve'
+}, {
+    id: 11,
+    img: 'https://archerythai.com/cdn/shop/products/202211315104312BZsLKQ9Wrf_grande.jpg?v=1677545264',
+    name: 'WIN&WIN wiawis ATF-DX',
+    price: 29500,
+    description: 'WIN&WIN wiawis ATF-DX Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Recurve'
+}, {
+    id: 12,
+    img: 'https://lancasterarchery.com/cdn/shop/products/X1033208.jpg?v=1671126513',
+    name: 'Hoyt Xceed',
+    price: 32500,
+    description: 'Hoyt Xceed Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Recurve'
+}, {
+    id: 13,
+    img: 'https://lancasterarchery.com/cdn/shop/files/1032807_Infrared.jpg?v=1704294843',
+    name: 'Hoyt Formula XD',
+    price: 32500,
+    description: 'Hoyt Formula XD Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Recurve'
+}, {
+    id: 14,
+    img: 'https://www.canadaarcheryonline.com/cdn/shop/files/Hoyt-GMX-3-Series-25-Grand-Prix-Recurve-Riser-2_grande.jpg?v=1692895780',
+    name: 'Hoyt GMX 3 Series',
+    price: 32500,
+    description: 'Hoyt GMX 3 Series Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Recurve'
+}, {
+    id: 15,
+    img: 'https://cdn.shopify.com/s/files/1/0550/8654/7130/products/4770437_black_600x600.jpg?v=1633774741',
+    name: 'WIN&WIN Wiawis Radical Pro',
+    price: 34500,
+    description: 'WIN&WIN Wiawis Radical Pro Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Recurve'
+}, {
+    id: 16,
+    img: 'https://www.canadaarcheryonline.com/cdn/shop/files/Win-Win-WIAWIS-Meta-DX-25-Recurve-Riser-5_grande.jpg?v=1682959918',
+    name: 'WIN&WIN Wiawis Meta DX',
+    price: 39500,
+    description: 'WIN&WIN Wiawis Meta DX Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Recurve'
+}, {
+    id: 17,
+    img: 'https://archerythai.com/cdn/shop/products/shibuyarcprodoubleblue_1024x1024@2x.jpg?v=1662537765',
+    name: 'Shibuya Ultima RC Pro Double Mount',
+    price: 18900,
+    description: 'Shibuya Ultima RC Pro Double Mount Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 18,
+    img: 'https://archerythai.com/cdn/shop/products/shibuyarcprosingleblue_1024x1024@2x.jpg?v=1662537615',
+    name: 'Shibuya Ultima RC Pro Single Mount',
+    price: 18900,
+    description: 'Shibuya Ultima RC Pro Single Mount Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 19,
+    img: 'https://archerythai.com/cdn/shop/products/shibuyacpxiiblue_1024x1024@2x.jpg?v=1618138515',
+    name: 'Shibuya Ultima GPX II',
+    price: 12500,
+    description: 'Shibuya Ultima GPX II Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 20,
+    img: 'https://archerythai.com/cdn/shop/products/Quarterto_1100x_116a4919-17f6-409f-a21b-d8d24ca4ec16_1024x1024@2x.png?v=1618552324',
+    name: 'Ultraview 3 Scope Set',
+    price: 12500,
+    description: 'Ultarview 3 Scope Set Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+},{
+    id: 21,
+    img: 'https://archerythai.com/cdn/shop/products/reactalpha_1024x1024@2x.jpg?v=1648442678',
+    name: 'Trophy Ridge React Alpha 1 PIN',
+    price: 7400,
+    description: 'Trophy Ridge React Alpha 1 PIN Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Bow Accessories'
+}, {
+    id: 22,
+    img: 'https://archerythai.com/cdn/shop/products/win_winsus_1024x1024@2x.jpg?v=1618382790',
+    name: 'WIN&WIN SUS V-BAR',
+    price: 1700,
+    description: 'WIN&WIN SUS V-BAR Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 23,
+    img: 'https://archerythai.com/cdn/shop/products/win_wincx-adblackgloss_1024x1024@2x.jpg?v=1618382082',
+    name: 'WIN&WIN CX-AD V-BAR',
+    price: 5100,
+    description: 'WIN&WIN CX-AD V-BAR Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 24,
+    img: 'https://archerythai.com/cdn/shop/products/acs-ellongrod_1024x1024@2x.jpg?v=1618373528',
+    name: 'WIN&WIN Wiawis ACS-EL LONG ROD',
+    price: 8900,
+    description: 'WIN&WIN Wiawis ACS-EL LONG ROD Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 25,
+    img: 'https://archerythai.com/cdn/shop/products/shibuyacarunoextenderred_1024x1024@2x.jpg?v=1618197413',
+    name: 'Shibuya Caruno Extender',
+    price: 2500,
+    description: 'Shibuya Caruno Extender Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 26,
+    img: 'https://archerythai.com/cdn/shop/products/blitz_1024x1024@2x.jpg?v=1648310371',
+    name: 'Trophy Ridge Blitz Stabilizer',
+    price: 1050,
+    description: 'Trophy Ridge Blitz Stabilizer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Bow Accessories'
+}, {
+    id: 27,
+    img: 'https://archerythai.com/cdn/shop/products/truballRave_1_2048x_648ae768-c560-4c38-94d2-4be535ca939d_1024x1024@2x.png?v=1618246535',
+    name: 'Truball Rave 3',
+    price: 5700,
+    description: 'Truball Rave 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 28,
+    img: 'https://archerythai.com/cdn/shop/files/FulkrumFlex_Main_2048x_e5e9d5ef-a71a-40bf-890a-fe7435e7e3f5_1024x1024@2x.webp?v=1693204412',
+    name: 'Truball Fulkrum FLEX',
+    price: 10500,
+    description: 'Truball Fulkrum FLEX Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 29,
+    img: 'https://archerythai.com/cdn/shop/products/ULTRAVIEWTHEHINGE2STAINLESS_1024x1024@2x.jpg?v=1652172867',
+    name: 'Ultraview The Hinge 2 Stainless',
+    price: 11900,
+    description: 'Ultraview The Hinge 2 Stainless Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 30,
+    img: 'https://archerythai.com/cdn/shop/products/eastonFlipside_4_Black_19_1024x1024@2x.jpg?v=1617615688',
+    name: 'Easton Flip Side 4-TUBE Quiver',
+    price: 1950,
+    description: 'Easton Flip Side 4-TUBE Quiver Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+},{
+    id: 31,
+    img: 'https://archerythai.com/cdn/shop/products/eastondeluxefieldquiverred_1024x1024@2x.jpg?v=1656407843',
+    name: 'Easton Deluxe Belt Field Quiver',
+    price: 3950,
+    description: 'Easton Deluxe Belt Field Quiver Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam necessitatibus quo qui, nam maiores.',
+    type: 'Bow Accessories'
+}, {
+    id: 32,
+    img: 'https://archerythai.com/cdn/shop/products/hexlight1_1024x1024@2x.jpg?v=1648443468',
+    name: 'Trophy Ridge Hex Light Quiver',
+    price: 3300,
+    description: 'Trophy Ridge Hex Light Quiver Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 33,
+    img: 'https://archerythai.com/cdn/shop/products/NeetDeerskin_1024x1024@2x.jpg?v=1618125285',
+    name: 'Neet Glove DG-1L Deerskin',
+    price: 1250,
+    description: 'Neet Glove DG-1L Deerskin Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}, {
+    id: 34,
+    img: 'https://archerythai.com/cdn/shop/products/INFITECEASYARMGUARDblack_1024x1024@2x.jpg?v=1675503492',
+    name: 'Infitec Easy Armguard',
+    price: 150,
+    description: 'Infitec Easy Armguard Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    type: 'Bow Accessories'
+}];
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                </div>`;
+    }
+    $("#productlist").html(html);
+
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+    // console.log('#'+elem.id)
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if( product[i].name.includes(value) ) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                </div>`;
+        }
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if(param == 'all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("."+param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name)
+    $("#mdd-price").text( numberWithCommas(product[index].price) + ' THB')
+    $("#mdd-desc").text(product[index].description)
+}
+
+function closeModal() {
+    $(".modal").css('display','none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if( productindex == cart[i].index ) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        // console.log(obj)
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
+    })
+    $("#cartcount").css('display','flex').text(cart.length)
+}
+
+function openCart() {
+    $('#modalCart').css('display','flex')
+    rendercart();
+}
+
+function rendercart() {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlist-items">
+                        <div class="cartlist-left">
+                            <img src="${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
+                                <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                                <p style="font-size: 1.2vw;">${ numberWithCommas(cart[i].price * cart[i].count) } THB</p>
+                            </div>
+                        </div>
+                        <div class="cartlist-right">
+                            <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                        </div>
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+}
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                    rendercart();
+                  }
+                })
+            }
+            rendercart();
+        }
+        
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
+    }
+}
